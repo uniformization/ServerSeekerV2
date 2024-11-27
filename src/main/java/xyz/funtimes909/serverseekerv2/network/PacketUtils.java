bfde161afb43563bbd23e6d6594eee9ee4f1d616
coolGi <me@coolgi.dev>
2024-11-27 14:04:03 +1030

    Refactored encryption & decryption of login packets

--- src/main/java/xyz/funtimes909/serverseekerv2/network/PacketUtils.java
+++ src/main/java/xyz/funtimes909/serverseekerv2/network/PacketUtils.java
@@ -4,14 +4,46 @@ import com.google.common.primitives.Bytes;
 import kotlin.Pair;
 import xyz.funtimes909.serverseekerv2.util.VarInt;
 
+import javax.crypto.BadPaddingException;
+import javax.crypto.Cipher;
+import javax.crypto.IllegalBlockSizeException;
 import java.io.IOException;
 import java.io.InputStream;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 public class PacketUtils {
     public static byte[] readStream(InputStream io) throws IOException {
         int size = VarInt.decode(io);
         return io.readNBytes(size);
+    }
+
+    /**
+     * Decrypts an encrypted input stream
+     * @param cipher The Cipher object already initialized with key & algorithm (used to decode)
+     * @return List of packets. While normal servers *should* only ever return one packet per request,
+     * it is possible to send back multiple. So this returns a list of the byte arrays which contains each packet.
+     */
+    public static List<byte[]> readEncryptedStream(InputStream io, Cipher cipher)
+            throws IOException, IllegalBlockSizeException, BadPaddingException
+    {
+        List<byte[]> result = new ArrayList<>();
+        byte[] encryptedPacket = io.readAllBytes();
+        byte[] packet = cipher.doFinal(encryptedPacket);
+
+        int packetStartIndex = 0;
+
+        while (packetStartIndex < packet.length) {
+            Pair<Integer, Byte> packetSize = VarInt.decode(packet, packetStartIndex);
+            packetStartIndex += packetSize.component1() + packetSize.component2();
+            result.add(Arrays.copyOfRange(
+                    packet,
+                    packetStartIndex - packetSize.component1() - 1,
+                    packetStartIndex - 1
+            ));
+        }
+
+        return result;
     }
 }
--- src/main/java/xyz/funtimes909/serverseekerv2/network/protocols/Login.java
+++ src/main/java/xyz/funtimes909/serverseekerv2/network/protocols/Login.java
@@ -53,6 +53,11 @@ public class Login {
                 OutputStream out = so.getOutputStream();
                 InputStream in = so.getInputStream();
                 ) {
+            // TODO: make this only run once
+            Security.addProvider(new BouncyCastleProvider());
+
+
+
             // The login request starts off with the Handshake and Login Start
             List<Byte> request = Handshake.getHandshake(protocol, "", (short) 0, (byte) 2);
             request.addAll(REQUEST);
@@ -85,53 +90,52 @@ public class Login {
             byte[] sharedSecret = new byte[16];
             SecureRandom.getInstanceStrong().nextBytes(sharedSecret);
             SecretKeySpec sharedSecretKey = new SecretKeySpec(sharedSecret, "AES");
+            IvParameterSpec sharedSecretIv = new IvParameterSpec(sharedSecret);
 //            System.out.println("Secret: " + Bytes.asList(sharedSecret));
 
 
+            Cipher decryptCipher = Cipher.getInstance("AES/CFB8/NoPadding", BouncyCastleProvider.PROVIDER_NAME);
+            decryptCipher.init(Cipher.DECRYPT_MODE, sharedSecretKey, sharedSecretIv);
+            Cipher encryptCipher = Cipher.getInstance("AES/CFB8/NoPadding", BouncyCastleProvider.PROVIDER_NAME);
+            encryptCipher.init(Cipher.ENCRYPT_MODE, sharedSecretKey, sharedSecretIv);
 
 
-            // TODO: make this only run once
-            Security.addProvider(new BouncyCastleProvider());
+
 
             KeyFactory keyFactory = KeyFactory.getInstance("X.509", BouncyCastleProvider.PROVIDER_NAME);
             X509EncodedKeySpec keySpec = new X509EncodedKeySpec(Bytes.toArray(publicKey.component1()));
             BCRSAPublicKey serverPublicKey = (BCRSAPublicKey) keyFactory.generatePublic(keySpec);
 
-//            System.out.println(serverPublicKey);
-//            System.out.println(Bytes.asList(serverPublicKey.getModulus().toByteArray()));
-
-            String encryptionAlgorithm = "RSA/None/PKCS1Padding";
-            byte[] encryptedSharedSecret = encrypt(encryptionAlgorithm, sharedSecret, serverPublicKey);
-            byte[] encryptedVerifyToken = encrypt(encryptionAlgorithm, Bytes.toArray(verifyToken.component1()), serverPublicKey);
-
-            // "AES/CFB8/PKCS5Padding"
+            Cipher serverEncryptCipher = Cipher.getInstance("RSA/None/PKCS1Padding", BouncyCastleProvider.PROVIDER_NAME);
+            serverEncryptCipher.init(Cipher.ENCRYPT_MODE, serverPublicKey);
 
 
             ArrayList<Byte> encryptionResponse = new ArrayList<>();
             // Protocol
             encryptionResponse.add((byte) 1);
+
             // Shared secret
+            byte[] encryptedSharedSecret = serverEncryptCipher.doFinal(sharedSecret);
+
             encryptionResponse.addAll(VarInt.encode(encryptedSharedSecret.length));
             encryptionResponse.addAll(Bytes.asList(encryptedSharedSecret));
+
             // Token
+            byte[] encryptedVerifyToken = serverEncryptCipher.doFinal(Bytes.toArray(verifyToken.component1()));
+
             encryptionResponse.addAll(VarInt.encode(encryptedVerifyToken.length));
             encryptionResponse.addAll(Bytes.asList(encryptedVerifyToken));
 
             // Prefix with size
             encryptionResponse.addAll(0, VarInt.encode(encryptionResponse.size()));
 
-            System.out.println("Encryption Response: " + encryptionResponse);
-            System.out.println("Encryption Response Size: " + encryptionResponse.size());
             out.write(Bytes.toArray(encryptionResponse));
 
 
 
-            byte[] serverReturn = in.readAllBytes();
+            byte[] serverReturn = PacketUtils.readEncryptedStream(in, decryptCipher).getFirst();
             System.out.println(Bytes.asList(serverReturn));
             System.out.println(new String(serverReturn));
-            byte[] decryptedServerReturn = decrypt("AES/CFB8/NoPadding", serverReturn, sharedSecretKey, new IvParameterSpec(sharedSecret));
-            System.out.println(Bytes.asList(decryptedServerReturn));
-            System.out.println(new String(decryptedServerReturn));
 //            byte[] buffer = new byte[1024];
 //            in.read(buffer);
 //            System.out.println(Bytes.asList(buffer));
@@ -170,25 +174,5 @@ public class Login {
 
     public static void main(String[] args) {
         System.out.println(login("127.0.0.1", (short) 25565));
-    }
-
-
-
-    public static byte[] encrypt(String algorithm, byte[] input, Key key) throws NoSuchPaddingException, NoSuchAlgorithmException,
-            InvalidAlgorithmParameterException, InvalidKeyException,
-            BadPaddingException, IllegalBlockSizeException, NoSuchProviderException {
-
-
-        Cipher cipher = Cipher.getInstance(algorithm, BouncyCastleProvider.PROVIDER_NAME);
-        cipher.init(Cipher.ENCRYPT_MODE, key);
-        return cipher.doFinal(input);
-    }
-    public static byte[] decrypt(String algorithm, byte[] cipherText, Key key, IvParameterSpec iv) throws NoSuchPaddingException, NoSuchAlgorithmException,
-            InvalidAlgorithmParameterException, InvalidKeyException,
-            BadPaddingException, IllegalBlockSizeException, NoSuchProviderException {
-
-        Cipher cipher = Cipher.getInstance(algorithm, BouncyCastleProvider.PROVIDER_NAME);
-        cipher.init(Cipher.DECRYPT_MODE, key, iv);
-        return cipher.doFinal(cipherText);
     }
 }
